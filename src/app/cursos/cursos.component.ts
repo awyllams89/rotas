c6b857c86a20ef60d880af4f601c3405682ab59c
awyllams89 <awyllams89@gmail.com>
2024-11-17 07:00:09 -0300

    Cria rotas com queryParams

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CursosService } from './cursos.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
@@ -9,11 +11,26 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos : any[]
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private service: CursosService) { }
+  constructor(private service: CursosService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.cursos = this.service.getCursos();
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+    this.router.navigate(['/cursos'],{queryParams:{'pagina': ++this.pagina}})
   }
 
 }
