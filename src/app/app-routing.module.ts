7e57cbe186fd15111abd99ca4b865e2d3c973ac4
awyllams89 <awyllams89@gmail.com>
2024-11-17 07:15:51 -0300

    Cria módulo de rotas pro curso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-  {path:'login', component:LoginComponent},
-  {path:'cursos', component:CursosComponent},
-  {path:'curso/:id', component:CursoDetalheComponent},
-  {path:'curso-nao-encontrado', component:CursoNaoEncontradoComponent},
+  {path:'login', component:LoginComponent}
 ];
 
 @NgModule({
--- src/app/cursos/cursos-routing.module.ts
+++ src/app/cursos/cursos-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cursos', component:CursosComponent},
+  {path:'curso/:id', component:CursoDetalheComponent},
+  {path:'curso-nao-encontrado', component:CursoNaoEncontradoComponent}]
+  ;
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
